8df117d8bbaa32c87e337b6d7cd59c03cc5dfec9
Dharani Manickavasagam <dhaaru1992@gmail.com>
2022-07-03 20:26:56 -0400

    ingredientUpdates (#4)

---
+++ src/app/ingredients/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient {
+  public name: string = '';
+  public count: number = 0;
+  constructor(name: string, count: number) {
+    this.name = name;
+    this.count = count;
+  }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../ingredients/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    { name: 'Apples', count: 50 },
+    { name: 'Roma Tomatoes', count: 10 },
+    { name: 'Bell Peppers', count: 5 }
+  ];
   constructor() {}
 
   ngOnInit(): void {}
