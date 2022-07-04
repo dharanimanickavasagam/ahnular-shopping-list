0ebbbde178ae95c7cc8c6f8649ccfaa95180a052
Dharani <dhaaru1992@gmail.com>
2022-07-03 20:25:32 -0400

    ingredientUpdates

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingListComponent } from '../shopping-list.component';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-  constructor() {}
+export class ShoppingEditComponent extends ShoppingListComponent {
+  constructor() {
+    super();
+  }
 
-  ngOnInit(): void {}
+  ingredientName: string = '';
+  ingredientCount: number = 0;
+
+  addIngredient = () => {
+    console.log('clicked', this.ingredients, {
+      name: this.ingredientName,
+      count: this.ingredientCount
+    });
+
+    this.ingredients.push({
+      name: this.ingredientName,
+      count: this.ingredientCount
+    });
+    console.log(this.ingredients);
+  };
 }
