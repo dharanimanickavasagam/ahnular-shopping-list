397749f8f9408e3738b0292eacd5cafd0817897f
Dharani <dhaaru1992@gmail.com>
2022-07-06 23:46:47 -0400

    recipeDetail Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,15 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() selectedRecipeEventEmitter = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'dharani',
       'just some name',
       'https://media.istockphoto.com/photos/vintage-cookbook-with-spices-and-herbs-on-rustic-wooden-background-picture-id1161153224?k=20&m=1161153224&s=612x612&w=0&h=dUAhyeGxsrNps3F10e28lOMadzJ8G50dJvwhdcoVJ4E='
     ),
-    new Recipe('dharani1', 'just some name1', 'hey you1')
+    new Recipe('dharani1', 'just some name1', 'hey you!')
   ];
-
   isAddNewRecipeButtonClicked: boolean = false;
   recipeName: string = '';
   recipeDescription: string = '';
@@ -23,8 +23,14 @@ export class RecipeListComponent implements OnInit {
 
   constructor() {}
 
+  ngOnInit(): void {}
+
+  onRecipeItemClick = (recipe: Recipe): void => {
+    console.log('emit', recipe);
+    this.selectedRecipeEventEmitter.emit(recipe);
+  };
+
   showReceipeInputFields = (): void => {
-    console.log('clicked');
     this.isAddNewRecipeButtonClicked = !this.isAddNewRecipeButtonClicked;
   };
 
@@ -45,6 +51,4 @@ export class RecipeListComponent implements OnInit {
     this.showReceipeInputFields();
     console.log(this.recipes);
   };
-
-  ngOnInit(): void {}
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  selectedRecipe: Recipe = { name: '', description: '', imagePath: '' };
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  updateSelectedRecipe = (data: Recipe) => {
+    this.selectedRecipe = data;
+    console.log('selectedRecipe', this.selectedRecipe);
+  };
 }
